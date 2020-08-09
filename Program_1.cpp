0bdd48f5c719bcbcc159794cb9ec16f0dbc73832
Chinmay <gargatechinmay@gmail.com>
2020-08-09 07:46:51 -0700

    first commit

---
+++ Program_1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>                         //This is header file
+using namespace std;                       //to use classes of function
+int main ()                                //this is main function
+{
+    int i =10;                             //this is integer value
+    char c ='a';                           //this is character value
+    string a ="Sherlock";                  //this is string value
+    float x =45.67;                        //this is float value
+    long int y =45641916;                  //this is long int value
+    double float z =4547.4545;             //this is double float value
+    cout<<"int value is"<<i<<"\n";         //this prints integer value
+    cout<<"char value is"<<c<<"\n";        //this prints character value
+    cout<<"string value is"<<a<<"\n";      //this prints string value
+    cout<<"float value is"<<x<<"\n";       //this prints float value
+    cout<<"long int value is"<<y<<"\n";    //this prints long int value
+    cout<<"double value is"<<z<<"\n";      //this prints double float value
+    return 0;                              //this is end of the main function
+
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# cpp_practice_programming
+## This is my First Repo
