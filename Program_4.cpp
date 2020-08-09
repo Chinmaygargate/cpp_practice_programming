cff6bb1957dbeec507fc1682d0cf9665272ec628
Chinmay <gargatechinmay@gmail.com>
2020-08-09 08:12:42 -0700

    Second commit

---
+++ Program_4.cpp
@@ -0,0 +1,7 @@
+#include<iostream>                                      //this is header file
+using namespace std;                                    //to use classes of function
+int main()                                              //this is main function
+{
+    for(int i = 10;i<20;i++)                            //Enter the integer values
+        cout<<"value of i is"<<i<<endl;                 //this prints i value
+}
