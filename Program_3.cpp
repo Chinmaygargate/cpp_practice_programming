7cd682a81e36f26b7399a63ab20e34037840ae9c
Chinmay <gargatechinmay@gmail.com>
2020-08-11 08:32:11 -0700

    Greater than program

---
+++ Program_3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>                     //This is header file
+using namespace std;                   //To use classes of function
+int main()                             //This is main function
+{
+    int a = 10, b = 20;                 //initialize the integer a and b values
+    if (a>b)                           // if a greater than b
+    {
+        cout<<"a is greater than b";          //This prints a is greater
+    }
+    else                               // otherwise
+        cout<<"b is greater than a";          //This prints b is greater
+        return 0;
+
+}
+
+
